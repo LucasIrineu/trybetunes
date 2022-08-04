062c21773483490152dcc83e8559efca0ea004ed
Lucas Irineu <lucasirineu119@gmail.com>
2022-08-03 21:12:06 -0300

    requisito 2

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,11 +7,37 @@ import Favorites from '../pages/Favorites';
 import Profile from '../pages/Profile';
 import ProfileEdit from '../pages/ProfileEdit';
 import NotFound from '../pages/NotFound';
+import Loading from './Loading';
 
 class Content extends React.Component {
+  constructor() {
+    super();
+    this.loadingStarted = this.loadingStarted.bind(this);
+    this.loadingEnded = this.loadingEnded.bind(this);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.loadingEnded();
+  }
+
+  loadingStarted() {
+    this.setState({ loading: true });
+  }
+
+  loadingEnded() {
+    this.setState({ loading: false });
+  }
+
   render() {
+    const { loading } = this.state;
+
     return (
       <div>
+        {loading && <Loading />}
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route exact path="/search" component={ Search } />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,109 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onLoginButtonClick = this.onLoginButtonClick.bind(this);
+    this.loadingStarted = this.loadingStarted.bind(this);
+    this.loadingEnded = this.loadingEnded.bind(this);
+
+    this.state = {
+      name: '',
+      isLoginButtonDisabled: true,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.loadingEnded();
+  }
+
+  handleLoginButtonDisable() {
+    const { name } = this.state;
+    const NAME_MIN_LENGTH = 3;
+
+    if (name.length >= NAME_MIN_LENGTH) {
+      this.setState({ isLoginButtonDisabled: false });
+    } else {
+      this.setState({ isLoginButtonDisabled: true });
+    }
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.handleLoginButtonDisable();
+    });
+  }
+
+  async onLoginButtonClick(event) {
+    event.preventDefault();
+
+    const { name } = this.state;
+    const { history } = this.props;
+    const { push } = history;
+
+    this.loadingStarted();
+    await createUser({ name });
+    this.loadingEnded();
+    push('/search');
+  }
+
+  loadingStarted() {
+    this.setState({ loading: true });
+  }
+
+  loadingEnded() {
+    this.setState({ loading: false });
+  }
+
   render() {
+    const { name, isLoginButtonDisabled, loading } = this.state;
+
     return (
-      <div data-testid="page-login">
-        Login
-      </div>
+      <section>
+        {loading && <Loading />}
+
+        <div data-testid="page-login">
+          <form>
+            <label htmlFor="input-name">
+              <input
+                type="text"
+                data-testid="login-name-input"
+                id="input-name"
+                name="name"
+                placeholder="Name"
+                value={ name }
+                onChange={ this.onInputChange }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="login-submit-button"
+              disabled={ isLoginButtonDisabled }
+              onClick={ this.onLoginButtonClick }
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
+      </section>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
